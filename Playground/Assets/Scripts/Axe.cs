350072eb9e80bdec53fa6f00b5a09f562abc3696
Savanna Smith <savanna@Joes-MacBook-Pro.local>
2019-01-19 14:13:44 -0500

    adding scripts folder

---
+++ Playground/Assets/Scripts/Axe.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axe : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionExit()
+    {
+      //TODO: if trigger is released
+      StartCoroutine("Disappear");
+    }
+
+    IEnumerator Disappear()
+    {
+      yield return new WaitForSeconds(5.0f);
+    }
+
+
+
+}
---
+++ Playground/Assets/Scripts/Target.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      ps = GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // What happens when the axe collides
+    void OnCollisionEnter(Collision col)
+    {
+      if (col.gameObject.CompareTag ("Axe"))
+      {
+        Debug.Log ("HIT!");
+        //What happens to the axe?
+        //col.gameObject.SetActive (false); //make axe disappear
+      }
+
+    }
+}
