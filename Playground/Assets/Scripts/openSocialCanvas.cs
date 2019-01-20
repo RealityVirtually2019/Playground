985ac713f2d247bf84a1c74886e9afee914a7905
shrma21294 <komalsharma21294@gmail.com>
2019-01-20 11:41:12 -0600

    added two canvas

---
+++ Playground/Assets/Scripts/openSocialCanvas.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openSocialCanvas : MonoBehaviour
+{
+
+    private IEnumerator time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = waitTime(45.0f);
+        StartCoroutine("waitTime");
+    }
+    
+    private IEnumerator waitTime(float time)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(time);
+        }
+    }
+}
